7598de244ad79fc22011ce5d473225433175698c
anmoljib <anmolhussainj@hotmail.com>
2026-01-19 14:51:00 +0100

    slette login, signup og supabase

--- supabase.js
+++
@@ -1,6 +0,0 @@
-import { createClient } from "https://esm.sh/@supabase/supabase-js@2";
-
-export const supabase = createClient(
-  "https://fsvasynwgxthvtzhwtkf.supabase.co",
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZzdmFzeW53Z3h0aHZ0emh3dGtmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Njg4MTQwMzQsImV4cCI6MjA4NDM5MDAzNH0.oTtgf9IMZsC4RSiXNCnCnGqnWYFAdz6s-2H_X79amSY"
-);
